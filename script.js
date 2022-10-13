d2963e81303ee35a93eb3ca641f68f3e8960eaee
Jesse Kucharek <111667621+jkucharek@users.noreply.github.com>
2022-10-13 13:57:49 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	document.getElementsByTagName('nav')[0].style.maxHeight = "0px";
+
+	document.getElementById('hamburger').addEventListener("click", function() {
+		if(document.getElementsByTagName('nav')[0].style.maxHeight == "0px")
+			document.getElementsByTagName('nav')[0].style.maxHeight = document.getElementsByTagName('nav')[0].scrollHeight + "px";
+		else
+			document.getElementsByTagName('nav')[0].style.maxHeight = "0px";
+	});
+
+};
